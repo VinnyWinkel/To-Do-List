15d715b9c4b1e9d4839be975ab20d358e0a1960d
Vinny Winkel <vinnywinkel@gmail.com>
2024-04-15 00:17:51 +0200

    click event of Input Appear created on "btn"

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 
+let inputBtn = document.getElementById("btn");
+let input = document.getElementById("input-container");
+
+inputBtn.addEventListener('click',inputAppear);
+
+function inputAppear(){
+    input.style.display = "block"
+}
