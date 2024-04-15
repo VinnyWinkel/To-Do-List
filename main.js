10c628d4825ae17cf1ca80fceb85f261973c5f1e
Vinny Winkel <vinnywinkel@gmail.com>
2024-04-15 17:33:14 +0200

    task styling complete, checkbox works, and line through works

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
 
 let inputBtn = document.getElementById("btn");
-let input = document.getElementById("input-container");
+let inputContainer = document.getElementById("input-container");
+let addBtn = document.getElementById("add-btn");
+let input = document.getElementById("input");
+let list = document.getElementById("list");
+
 
 inputBtn.addEventListener('click',inputAppear);
 
 function inputAppear(){
-    input.style.display = "block"
+    inputContainer.style.display = "block"
 }
+
+addBtn.addEventListener('click',addTask);
+
+function addTask(){
+
+    let inputValue = input.value
+
+    let newTask = document.createElement("li")
+    let icon = document.createElement("i");
+    newTask.innerHTML = `<i class="fa-regular fa-square"></i>${inputValue}`
+    newTask.addEventListener('click',click)
+    list.appendChild(newTask)
+
+   function click(){
+    if(newTask.firstElementChild.className === "fa-regular fa-square"){
+        newTask.firstElementChild.className = "fa-solid fa-square-check" 
+        newTask.style.textDecoration = "line-through"
+    }
+    else{
+        newTask.firstElementChild.className = "fa-regular fa-square"
+        newTask.style.textDecoration = "none" 
+    }
+} 
+
+ 
+
+ 
+}
+
+
+
